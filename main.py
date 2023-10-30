59875efd8f41866bb9879e68d68688722c795d8a
AltaJD <altair231002@gmail.com>
2023-10-30 11:51:48 +0800

    1. Removed bug of incorrectly reading the config.yaml 2. Refactored code

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import sys
 import config_extractor as config
 
 if __name__ == '__main__':
-    start: tuple = config.get('initial_position')
-    goal:  tuple = config.get('final_position')
-    safety_distance: float = config.get('safety_distance')
-    print(start, goal, safety_distance)
-    filename = 'data_storage/colliders.csv'
-    grid_filename = 'data_storage/grid.csv'
+    """ === Get configuration === 
+    It includes default data for testing the system, such as:
+    1. Start coordinate
+    2. Goal coordinate
+    3. Safety distance for wheelchair
+    4. Path to the colliders coordinates
+    5. Path to save the map as csv file for better view of the array and debugging
+    """
+    start:              tuple = config.get('initial_position')
+    goal:               tuple = config.get('final_position')
+    safety_distance:    float = config.get('safety_distance')
+    filename:           str   = config.get('colliders')
+    grid_filename:      str   = config.get('grid_save')
+
+    """ === Map generation === """
     grid, skeleton, distances, original_data = map.get_custom_map(filename=filename, safety_distance=safety_distance)
     # map.save_grid(grid_filename, grid)
     map.normalized_grid(grid) # inplace = True
@@ -23,6 +32,7 @@ if __name__ == '__main__':
     print(10 * '=' + f"Distances" + "=" * 10)
     pprint(distances)
     map.show_map(grid, skeleton, start, goal)
+    """ === Path planning === """
     # path, waypoint_values = map.get_path(grid=grid, start=start, goal=goal)
     # if len(path) == 0:
     #     print('Path was not found.', file=sys.stderr)
